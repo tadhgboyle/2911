69d18fe5565ce07ede1b641bf0550acb6f20d586
Tadhg <tadhgsmboyle@gmail.com>
2021-04-28 10:20:53 -0700

    :construction: Create basic view layouts and include structure

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Working on Expenses
+
+## Running the Flask server
 - cd into directory and run `source venv/bin/activate`
 - (if first time working) run `pip3 install Flask`
 - type `export FLASK_APP=main.py`
+- type `export FLASK_ENV=development` (this will reload the flask server when you save code changes)
 - type `flask run`
 - try not to break anything!
+
+## About HTML pages
+- to reduce reused code, all our html pages will "extend" the base html page `templates/layout.html`
+    - this layout page has the doctype, header, and basic page layout tags already
+- all pages for each section of the app are in `templates/views/` directory
+    - for example, the List Expenses page is `templates/views/list_expenses.html`
--- routes.py
+++ routes.py
@@ -4,44 +4,39 @@ from flask import render_template
 
 @app.route('/')
 def expenses_list():
-   return 'Your expenses'
+   return render_template('views/list_expenses.html', page_title='List Expenses')
 
 
 @app.route('/add-expense')
 def add_expense():
-    return 'Add expense'
+    return render_template('views/add_expense.html', page_title='Add Expense')
 
 
 @app.route('/edit-expense')
 def edit_expense():
-    return 'Edit expense'
+    return render_template('views/edit_expense.html', page_title='Edit Expense')
 
 
 @app.route('/delete-expense')
 def delete_expense():
-    return 'Delete expense'
+    return render_template('views/delete_expense.html', page_title='Delete Expense')
 
 
 @app.route('/add-category')
 def add_category():
-    return 'Add category'
+    return render_template('views/add_category.html', page_title='Add Category')
 
 
 @app.route('/edit-category')
 def edit_category():
-    return 'Edit category'
+    return render_template('views/edit_category.html', page_title='Edit Category')
 
 
 @app.route('/delete-category')
 def delete_category():
-    return 'Delete category'
+    return render_template('views/delete_category.html', page_title='Delete Category')
 
 
 @app.route('/statistics')
 def statistics():
-    return 'Statistics'
-
-
-@app.route('/template')
-def test():
-    return render_template('test.html', user={'first_name': 'Tadhg', 'last_name': 'Boyle'})
+    return render_template('views/statistics.html', page_title='Statistics')
