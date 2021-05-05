be697a652af25cbb9426cdc5b16a89aa98a79e5d
Tadhg <tadhgsmboyle@gmail.com>
2021-05-05 10:49:44 -0700

    :bug: Fix expense min value + 2 decimal formatting when editing expense amount

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ from models import Category
 class ExpenseForm(Form):
     name = StringField('name', [validators.Length(min=3, max=64)])
     category_id = SelectField('category_id', [validators.Length(min=24, max=24)], validate_choice=False)
-    amount = FloatField('amount', [validators.NumberRange(min=0)])
+    amount = FloatField('amount', [validators.NumberRange(min=0.01)])
     date = DateField('date')
     # used when editing a expense to find it from the database, ignored when making new expense
     expense_id = StringField('expense_id')
