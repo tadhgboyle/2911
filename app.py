41b332277dd1f29dff5757537e97ef321348f82c
Manraj Bhullar <59130576+manrajbhullar@users.noreply.github.com>
2021-05-07 10:52:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,9 @@ def create_app():
     def list_expenses():
         return expense_controller.list_expenses()
 
-
     @app.route('/add-expense', methods=['GET', 'POST'])
     def add_expense():
         return expense_controller.add_expense()
-
 
     @app.route('/edit-expense/<expense_id>', methods=['GET', 'POST'])
     def edit_expense(expense_id):
@@ -30,26 +28,21 @@ def create_app():
     def delete_expense(expense_id):
         return expense_controller.delete_expense(expense_id)
 
-
     @app.route('/list-categories')
     def list_categories():
         return category_controller.list_categories()
-
 
     @app.route('/add-category', methods=['GET', 'POST'])
     def add_category():
         return category_controller.add_category()
 
-
     @app.route('/edit-category/<category_id>', methods=['GET', 'POST'])
     def edit_category(category_id):
         return category_controller.edit_category(category_id)
 
-
     @app.route('/delete-category/<category_id>')
     def delete_category(category_id):
         return category_controller.delete_category(category_id)
-
 
     @app.route('/statistics')
     def statistics():
